784d6e560ffe13c0b0669449d807ff4b50a42cdc
Santiago Galardi <santygalardi@gmail.com>
2023-12-14 13:16:51 -0300

    cv update

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
   description:
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   github: 'https://github.com/santigalardi',
-  resume: 'https://drive.google.com/file/d/16Lq0mOVoq_0MviftZHDV7F8LxBSspBML/view?usp=drive_link',
+  resume: 'https://drive.google.com/file/d/1M8bZSQpotaIYwV1Q6yu3dhhb65enpNaA/view?usp=drive_link',
   linkedin: 'https://www.linkedin.com/in/santigalardi/',
 };
 
